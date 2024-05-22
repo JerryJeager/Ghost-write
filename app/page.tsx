57c5662a038cd015dfab6cbe72f256268626a5fd
Amadi Jerry <amadijerry310@gmail.com>
2024-05-22 13:03:48 +0100

    add logo icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-
+import ghost from "../public/images/ghost-write-icon.png"
+import Image from "next/image"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1 className="text-green-700">Ghost Write</h1>
+      <Image alt="icon image" src={ghost} width={200} />
     </main>
   );
 }
