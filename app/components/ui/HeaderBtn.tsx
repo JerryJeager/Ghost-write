ba67980f94338577782dc3b91a1652ce194db1bd
Amadi Jerry <amadijerry310@gmail.com>
2024-05-23 09:24:29 +0100

    remove login button in dashboard

--- app/components/ui/HeaderBtn.tsx
+++ app/components/ui/HeaderBtn.tsx
@@ -1,11 +1,21 @@
-"use client"
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const HeaderBtn = () => {
+  const pathname = usePathname();
   return (
     <>
-        <button className="bg-primary p-1 lg:p-2 rounded-md text-white w-20">Login</button>
+      {!pathname.includes("dashboard") && (
+        <Link href={"/auth/login"}>
+          <button className="bg-primary p-1 lg:p-2 rounded-md text-white w-20">
+          Login
+        </button>
+        </Link>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HeaderBtn
+export default HeaderBtn;
